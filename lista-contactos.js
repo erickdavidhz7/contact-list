06978c9961a55110852c9e5d04e03ff0708cf353
Erick Henriquez <erickdavid7@hotmail.com>
2023-06-24 20:37:01 -0500

    Se ha añadido la función para actualizar contacto y mejorado en general el programa.

--- lista-contactos.js
+++ lista-contactos.js
@@ -1,25 +1,31 @@
 // Definiendo la lista de contactos como un array con un contacto ingresado para cuestiones de testeo.
 const listaContactos = [{
-    id: 1,
+    id: 123456,
     nombre: "Erick",
     apellido: "Henríquez",
-    telefono: 3016962090,
+    telefono: "321-4567890",
     ubicacion: {
-        ciudad: "Medellín",
-        direccion: "por la 33a con 78a"
+        ciudad: "En un lugar con montañas",
+        direccion: "por ahí"
     }
 }];
 // Función para añadir un contacto a la lista de contactos.
 const añadirContacto = (nombreApellido, telefonoIngresado, ciudadIngresado, direccionIngresado) =>{
-    let idContador = listaContactos.length + 1;
+    // Se han definido un rango minimo y máximo para generar el número del ID que es de 6 digitos
+    const idMin = 100000;
+    const idMax = 199999;
+    // Se genera un número aleatorio entre 100,000 y 199,999 para generar el ID
+    let idContador = Math.floor(Math.random() * (idMax - idMin) + idMin);
+    // Se separa el nombre y el apellido
     nombreApellido = nombreApellido.split(" ");
     let nombreIngresado = nombreApellido[0];
     let apellidoIngresado = nombreApellido[1];
 
+    // Se itera sobre la lista de contactos
     for (const contacto of listaContactos) {
         if(contacto.nombre == nombreIngresado && contacto.apellido == apellidoIngresado) {
             console.log(`El contacto "${nombreIngresado} ${apellidoIngresado}" ya se encuentra en la lista de contactos.`);
-            return; // parar la función porque ya se ha encontrado el usuario.
+            return; // parar la función porque ya se ha encontrado el contacto.
         }
     }
     // Se define un nuevo objeto contacto con los datos ingresados por el usuario como valores de los atributos.
@@ -39,26 +45,51 @@ const añadirContacto = (nombreApellido, telefonoIngresado, ciudadIngresado, dir
 }
 // Functión para borrar contacto a la lista de contactos.
 const borrarContacto = (contactoEliminar) =>{
+    // Se separa el nombre y el apellido
     contactoEliminar = contactoEliminar.split(" ");
     let nombreIngresado = contactoEliminar[0];
     let apellidoIngresado = contactoEliminar[1];
 
+    // Se itera sobre la lista de contactos
     for (const contacto of listaContactos){
         if(contacto.nombre == nombreIngresado && contacto.apellido == apellidoIngresado) {
             const index = listaContactos.indexOf(contacto);
             listaContactos.splice(index, 1);
             console.log(`El contacto "${nombreIngresado} ${apellidoIngresado}" ha sido eliminado de la lista de contactos.`);
-            return;
+            return; // parar la función porque ya se ha encontrado el contacto y se ha eliminado.
         }
     }
-    console.log(`No se ha encontrado el contacto "${nombreIngresado} ${apellidoIngresado}" en la lista de contactos, por favor ingrese un contacto presente en la lista.`);
+    console.log(`No se ha encontrado el contacto "${nombreIngresado} ${apellidoIngresado}" en la lista de contactos.`);}
+
+// Función para actualizar un contacto.
+const actualizarContacto = (idIngresado, contactoActualizar, telefonoIngresado, ciudadIngresado, direccionIngresado) =>{
+    // Se separa el nombre y el apellido
+    contactoActualizar = contactoActualizar.split(" ");
+    let nombreIngresado = contactoActualizar[0];
+    let apellidoIngresado = contactoActualizar[1];
+    
+    for (const contacto of listaContactos){
+        if(contacto.id == idIngresado) {
+            contacto.nombre = nombreIngresado;
+            contacto.apellido = apellidoIngresado;
+            contacto.telefono = telefonoIngresado;
+            contacto.ubicacion.ciudad = ciudadIngresado;
+            contacto.ubicacion.direccion = direccionIngresado;
+            console.log(`Se ha actualizado los datos del contacto "${nombreIngresado} ${apellidoIngresado}" en la lista de contactos.`);
+            return; // parar la función porque ya se ha encontrado el contacto y se ha eliminado.
+        }
+    }
+    console.log(`No se ha encontrado el contacto "${nombreIngresado} ${apellidoIngresado}" en la lista de contactos.`);
 }
+
 // Función para imprimir lista de contactos.
 const imprimirListaContactos = ()=>{
     console.log("La lista de contactos\nTiene los siguientes contactos:");
     for (const contacto of listaContactos) {
-        console.log(`-Contacto-\nID: ${contacto.id}\nNombre: ${contacto.nombre}\nApellido: ${contacto.apellido}\nTeléfono: ${contacto.telefono}\nCiudad: ${contacto.ubicacion.ciudad}\nDirección: ${contacto.ubicacion.direccion}`);
-        console.log('-------------------' );
+        console.log('---------------------------' );
+        console.log(`-Contacto-\nID: ${contacto.id}\nNombre: ${contacto.nombre}\nApellido: ${contacto.apellido}`);
+        console.log(`Teléfono: ${contacto.telefono}\nCiudad: ${contacto.ubicacion.ciudad}\nDirección: ${contacto.ubicacion.direccion}`);
+        console.log('---------------------------' );
 
     }
 }
@@ -68,11 +99,19 @@ function main(){
     // Llamando las respectivas funciones para testeo.
     imprimirListaContactos();
     añadirContacto("Yeison Chanel", "333-3015212", "Santo Tomas", "La 20 de Julio");
+    // Cuando un contacto ya se ha añadido
     añadirContacto("Yeison Chanel", " 301-0515656", "Santo Tomas", "La 20 de Julio");
+    // Añadiendo 2 contactos más para la visualización cuando se impriman los contactos
     añadirContacto("David Perez", "333-3444559", "Soledad", "Exito del guazimo");
     añadirContacto("Lautaro Javier", "330-0145755", "Bello", "La chinita");
     borrarContacto("David Perez");
+    // Cuando un contacto no existe en la lista y se intenta eliminar
     borrarContacto("Lautaro Martinez");
+    console.log('');
+    imprimirListaContactos();
+    console.log('');
+    actualizarContacto(123456, "Erick Henríquez", "301-6962090", "Medellín", "Calle 33a #78a-72");
+    console.log('');
     imprimirListaContactos();
 }
 
